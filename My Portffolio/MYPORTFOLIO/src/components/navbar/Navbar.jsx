d95d6d7561ac582d5d2da9b9900360e6bfba24f0
João Victor <fernandesjoaovictor836@gmail.com>
2025-09-03 12:25:43 -0300

    TailwindCSS added on portfolio.

--- My Portffolio/MYPORTFOLIO/src/components/navbar/Navbar.jsx
+++ My Portffolio/MYPORTFOLIO/src/components/navbar/Navbar.jsx
@@ -1,31 +1,64 @@
+import { useState } from "react";
 import styles from "./Navbar.module.css";
 
-const Navbar = () => {
+function Navigation() {
   return (
-    <div className={styles.container}>
-      <nav>
-        <ul>
-          {/* li's from navbar */}
-          <a href="/">
-            <li>Start</li>
+    <ul className="nav-ul">
+      <li className="nav-li">
+        <a href="#home" className="nav-link">
+          Home
+        </a>
+      </li>
+      <li className="nav-li">
+        <a href="#home" className="nav-link">
+          About me
+        </a>
+      </li>
+      <li className="nav-li">
+        <a href="#home" className="nav-link">
+          Tech Stack
+        </a>
+      </li>
+      <li className="nav-li">
+        <a href="#home" className="nav-link">
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-li">
+        <a href="#home" className="nav-link">
+          Experience
+        </a>
+      </li>
+      <li className="nav-li">
+        <a href="#home" className="nav-link">
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40">
+      <div className="mx-auto c-space max-w-7xl">
+        <div className="flex items-center justify-between py-2 sm:py-0">
+          <a
+            href=""
+            className="text-xl font-bold transition-colors text-neutral-400 hover:text-white"
+          >
+            John
           </a>
-          <a href="/about">
-            <li>About me</li>
-          </a>
-          <a href="/techstack">
-            <li>Tech Stack</li>
-          </a>
-          <a href="/portfolio">
-            <li>Portfolio</li>
-          </a>
-          <a href="/experience">
-            <li>Experience</li>
-          </a>
-          <a href="/contact">
-            <li>Contact</li>
-          </a>
-        </ul>
-      </nav>
+          <button className="flex cursor-pointer text-neutral-400 hover:text-white">
+            <img
+              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
+              alt="toggle"
+              className="w-6 h-6"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
